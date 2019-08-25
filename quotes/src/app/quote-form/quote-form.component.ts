a4f9665e08f54ee921c6717ce653ed84f53962de
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-25 12:25:17 +0200

    added some quotes form codes in ts

---
+++ quotes/src/app/quote-form/quote-form.component.ts
@@ -0,0 +1,44 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-quote-form',
+//   templateUrl: './quote-form.component.html',
+//   styleUrls: ['./quote-form.component.css']
+// })
+// export class QuoteFormComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import { globalAgent } from 'http';
+// import { Quote } from '@angular/compiler';
+import { Quote} from '../quote';
+// import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-quote-form',
+  templateUrl: './quote-form.component.html',
+  styleUrls: ['./quote-form.component.css']
+})
+export class QuoteFormComponent implements OnInit {
+
+  newQuote=new Quote(0,"","","",new Date(),0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0,"","","",new Date(),0,0)
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
