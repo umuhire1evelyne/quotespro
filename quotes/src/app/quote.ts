5dbc29f523f6b23e81b2ac5c1c5ce9af0c1a5292
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-08-25 11:50:04 +0200

    added constructor

---
+++ quotes/src/app/quote.ts
@@ -0,0 +1,19 @@
+// export class Quote {
+// }
+
+export class Quote {
+    showDescription:boolean;
+    constructor(
+      public id: number,
+      public name: string,
+      public theQuote: string,
+      public author: string,
+      public postDate:Date,
+      public numberOfLikes:number, 
+      public numberOfDislikes:number)
+      {
+        this.showDescription=false;
+      }
+      
+    
+  }
